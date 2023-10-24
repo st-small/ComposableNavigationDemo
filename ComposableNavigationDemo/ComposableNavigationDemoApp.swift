5156a0af6bd0f8b877bd66a34b3fffd883769826
Stanly Shianovskiy <st-small@rambler.ru>
2023-10-24 19:42:19 +0300

    Initial Commit

---
+++ ComposableNavigationDemo/ComposableNavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableNavigationDemoApp.swift
+//  ComposableNavigationDemo
+//
+//  Created by Stanly Shiyanovskiy on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ComposableNavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
